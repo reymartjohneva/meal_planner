cff9353a5c244afb1c3ae72c8ff34cf27bec240c
reymartjohneva <mayestroga3@gmail.com>
2025-05-30 23:14:19 +0800

    feat: last nani

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz_init;
+import 'package:permission_handler/permission_handler.dart';
 
 class NotificationService {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
